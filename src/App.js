e2376d2a16e8ff6d2e8d71f115372b7f7004398f
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-04-23 20:55:39 -0700

    jay erica test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
-        <h2>Hello erica</h2>
+        <h1>I am hungry</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
