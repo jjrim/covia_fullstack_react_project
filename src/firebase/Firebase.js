4a14e11b76e22d3fdf4883c2b1df1e97fc5a74f8
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-04-28 02:19:44 -0700

    login attemp final

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,16 +1,16 @@
-  import firebase from "firebase";
+//   import firebase from "firebase";
   
-  var firebaseConfig = {
-    apiKey: "AIzaSyB0KQUuIHKY-SIKWR0Y2vMvkHg2NMJNyNQ",
-    authDomain: "covia-f2018.firebaseapp.com",
-    databaseURL: "https://covia-f2018.firebaseio.com",
-    projectId: "covia-f2018",
-    storageBucket: "covia-f2018.appspot.com",
-    messagingSenderId: "323853360394",
-    appId: "1:323853360394:web:f29faa62a94a7b729deef3",
-    measurementId: "G-CV8H9PL5BZ"
-  };
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyB0KQUuIHKY-SIKWR0Y2vMvkHg2NMJNyNQ",
+//     authDomain: "covia-f2018.firebaseapp.com",
+//     databaseURL: "https://covia-f2018.firebaseio.com",
+//     projectId: "covia-f2018",
+//     storageBucket: "covia-f2018.appspot.com",
+//     messagingSenderId: "323853360394",
+//     appId: "1:323853360394:web:f29faa62a94a7b729deef3",
+//     measurementId: "G-CV8H9PL5BZ"
+//   };
 
-const firebase = firebase.initialize(firebaseConfig)
+// const Firebase = firebase.initialize(firebaseConfig)
 
-export default firebase;
+// export default Firebase;
