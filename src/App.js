4eb5d79c49212154a675f42348f0c2d43864e89b
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-04-27 22:27:20 -0700

    main page logo test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-        <h1>I am hungry</h1>
-        <h1>I am aaa</h1>
-        <h1>I hate project</h1>
-        <h1>Hello</h1>
-        <h1>hehke</h1>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p>COVIA FOREVER</p>
         <a
           className="App-link"
           href="https://reactjs.org"
