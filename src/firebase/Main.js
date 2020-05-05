743d106915d6cf67d263a06df71fd349b806bbac
Bmeimei <skystar505836915@gmail.com>
2020-05-04 19:13:18 -0700

    Add pages, and change 'Button' tag to 'Link' tag

--- src/firebase/Main.js
+++ src/firebase/Main.js
@@ -3,6 +3,10 @@ import React , { Component } from "react";
 import fire from "./fire";
 import "../homepage.css";
 import logo from './logo.png';
+import { BrowserRouter as Router, Route, Link , Switch} from 'react-router-dom';
+import { Single } from './Single';
+import { Two } from './Two';
+import { Statistics } from './Statistic';
 class Main extends Component {
 constructor(props)
     {
@@ -42,23 +46,23 @@ render()
     }
     else{
     return(
-
+        <Router>
     <div className="homepage-content">
         <img src={logo} className="logo" alt="logo" height="130" width="200"/>
         <h3>Coronavirus Cases In the World</h3>
         <h2>{number}</h2>
         <div className="home-buttons">
-            <button className="home-button">Single-Player</button>
-            <button className="home-button">Two Player</button>
-            <button className="home-button">Rules</button>
-            <button className="home-button">Statistics</button>
-            <button className="home-button">Create Custom Question</button>
-            <button onClick={this.logout} className="home-button">Logout</button>
+            <Link className="home-button">Single-Player</Link>
+            <Link className="home-button">Two Player</Link>
+            <Link className="home-button">Rules</Link>
+            <Link className="home-button">Statistics</Link>
+            <Link className="home-button">Create Custom Question</Link>
+            <Link onClick={this.logout} className="home-button">Logout</Link>
         </div>
         
 
     </div>
-
+    </Router>
     )
 }
     }
