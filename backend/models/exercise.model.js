0cbe65b69f52053b9554036dd2e5cf13a7450dbb
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-05-14 15:44:44 -0700

    fixed exercise model to question model

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
   username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
+  question: { type: String, required: true },
+  option1: { type: String, required: true },
+  option2: { type: String, required: true },
+  option3: { type: String, required: true },
+  answer: { type: String, required: true },
 }, {
   timestamps: true,
 });
