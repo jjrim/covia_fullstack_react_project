98d4442210d0abc782b848df4d46ed8901b77709
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-05-14 01:49:45 -0700

    added router to question.js

--- src/firebase/Question.js
+++ src/firebase/Question.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function Question() {
     return (
+        <Router>
         <div>
             I am question page
         </div>
+        </Router>
     )
 }
