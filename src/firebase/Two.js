fe759e27066d447c07cf892a92edec30c3bf2a48
Bmeimei <skystar505836915@gmail.com>
2020-05-14 20:56:04 -0700

    change port

--- src/firebase/Two.js
+++ src/firebase/Two.js
@@ -10,7 +10,7 @@ import { QuizData } from './Twoquizdata';
 import { Icon } from 'semantic-ui-react'
 import leeke from './leeke.png'
 
-const PORT = 'localhost:5000/'
+const PORT = process.env.PORT
 let socket = io(PORT)
 export default class Two extends Component {
     constructor(props){
