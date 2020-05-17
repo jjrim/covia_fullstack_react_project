0d86bd4dcff9a55979ea61f8cf9a6486ff611c02
dafu2020 <xlu49@my.bcit.ca>
2020-05-17 14:51:47 -0700

    debugged 2player join function

--- src/firebase/EasterEgg.js
+++ src/firebase/EasterEgg.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 
+function refreshPage() {
+    window.location.reload(false);
+}
+
 
 function Square(props) {
     return (
@@ -8,6 +12,27 @@ function Square(props) {
             {props.value}
         </button>
     );
+}
+
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 class Board extends React.Component {
@@ -117,27 +142,6 @@ class Game extends React.Component {
     }
 }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
-function refreshPage() {
-    window.location.reload(false);
-  }
+
 
 export default Game;
