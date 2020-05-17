fdec68cb4979d9265f5a4bdb5a9e7837fb13cac8
Bmeimei <skystar505836915@gmail.com>
2020-05-17 10:44:44 -0700

    Improve everything about Multiplayer

--- src/firebase/Multi.js
+++ src/firebase/Multi.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Link , Switch } from 'react-router-dom'
 import Join from './Join'
 import Two from './Two'
 import Main from './Main';
+import Single from './Single';
+import Statistics from './Statistic';
+import Question from './Question';
 
 export default class Multi extends Component {
     render() {
@@ -12,6 +15,9 @@ export default class Multi extends Component {
             <Route path='/' component={Main} exact></Route>
             <Route path='/mul/Join' component={Join} exact></Route>
             <Route path='/mul/Two' component={Two} exact></Route>
+            <Route path='/Single' component={Single} exact></Route>
+            <Route path='/Statistics' component={Statistics} exact></Route>
+            <Route path='/Question' component={Question} exact></Route>
         </Router>
             </div>
         )
