30d71a35f51b794f4db6e51b1af1e1ad96187b6b
Bmeimei <skystar505836915@gmail.com>
2020-05-17 02:22:02 -0700

    MU Finished

--- src/firebase/Multi.js
+++ src/firebase/Multi.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link , Switch } from 'react-router-dom';
 import Join from './Join'
 import Two from './Two'
-
+import Main from './Main';
 
 export default class Multi extends Component {
     render() {
         return (
             <div>
         <Router> 
+            <Route path='/' component={Main} exact></Route>
             <Route path='/mul/Join' component={Join} exact></Route>
             <Route path='/mul/Two' component={Two} exact></Route>
         </Router>
