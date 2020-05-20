eb76c63f2a345c659b36bfa01f0ed6d3ef8e129a
Bmeimei <skystar505836915@gmail.com>
2020-05-20 10:27:53 -0700

    jump to homepage after delete

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -17,7 +17,7 @@ const Exercise = props => (
 
 
     <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <Link to='/' onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</Link>
     </td>
   </tr>
 )
