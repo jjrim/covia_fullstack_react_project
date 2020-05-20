e5a29ee8a9435510e130e925c198335024336800
dafu2020 <xlu49@my.bcit.ca>
2020-05-19 17:55:26 -0700

    add go back and quit btn for navbar.component.js

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -8,16 +8,22 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="ui container" id = "createOrViewQuestionDive">
+        <div>
+          <WrittingQuestion />
+        </div>
         <div id = "createOrViewLink" className="ui container">  
           <Link to="/create"> <button className = "ui teal button" id = "createQuestionBtn">Create Custom Question</button></Link>
           <br/>
           <Link to="/"> <button className = "ui blue button" id= "viewQuestionBtn" >View List of Questions</button></Link>
+          <br/>
         </div>
 
-        <div>
-          <WrittingQuestion />
-
+        <div> 
+          <button className = "ui blue inverted small button" id = "createPageGoBackBtn" >Back</button>
+          <button className = "ui blue inverted small button" >Sign Out</button>
         </div>
+
+
       </div>
     );
   }
